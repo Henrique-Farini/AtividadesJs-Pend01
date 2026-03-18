de12050eb5a71d91471a1afb9491e4c9a740e455
Henrique-Farini <dalrefar@gmail.com>
2026-03-18 14:19:51 -0300

    secondcommit

--- JavaScript/DOM/atividades/atividades.js
+++ JavaScript/DOM/atividades/atividades.js
@@ -36,3 +36,49 @@ clicar.addEventListener("click", function () {
     contadorzim++; // Aumenta o número
     contarclique.textContent = "Cliques: " + contadorzim; 
 });
+
+//atividade 5
+let txt = document.getElementById("ainserir");
+let adicionar = document.getElementById("adicionar");
+let lista = document.getElementById("lista")
+
+adicionar.addEventListener("click", function() {
+    let itemnalista = document.createElement("li")
+    itemnalista.textContent = txt.value;
+    lista.appendChild(itemnalista);
+    txt.value = '';
+    //atividade 6
+    itemnalista.addEventListener("click", function() {
+    // Substitui o confirm nativo pelo SweetAlert2
+    Swal.fire({
+        title: 'Deseja remover?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, remover!', // Altere aqui o texto do OK
+        cancelButtonText: 'Cancelar'        // Altere aqui o texto do Cancelar
+    }).then((result) => {
+        // Se o usuário clicou no botão de confirmação
+        if (result.isConfirmed) {
+            lista.removeChild(itemnalista);
+            // Opcional: mostrar um alerta de sucesso
+            Swal.fire('Removido!', 'O item saiu da lista.', 'success');
+        }
+    });
+});
+});
+
+    //HARD
+
+    let nome = document.getElementById("nome");
+    let enviar = document.getElementById("enviar");
+    let nomes = document.getElementById("paragrafo");
+
+    enviar.onclick = function() {
+        if (nome.value === "") {
+            nomes.style.color = "red";
+            nomes.textContent = "O campo nome é obrigatório";
+            return;
+        }
+    };
